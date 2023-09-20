bc4e55cde502f2bab7d5df6632fa3713c54777a0
Nziza Samuel <nziza.samuel@student.moringaschool.com>
2023-09-20 19:47:04 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return 'This one is on me!';
+  } else if (feet > 2000 && feet <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else if (feet > 2500) {
+    return 'No can do.';
+  } else {
+    return 'This is not a valid distance.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go."
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  return tip === "generous" ? "Thank you so much." : tip === "not as generous" ? "Thank you." : "Bye."
 }
